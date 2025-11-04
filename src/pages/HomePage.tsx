009b059fd4af9aa5db0bcf7992878de8c596dbc7
sahaiestates03-spec <sahaiestates03@gmail.com>
2025-11-04 14:02:31 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,12 +9,20 @@ export default function HomePage() {
   return (
     <div className="overflow-x-hidden">
       <RotatingHero
-        images={["/BG-IMG-1.jpg","/BG-IMG-2.jpg","/BG-IMG-3.jpg","/BG-IMG-4.jpg","/BG-IMG-5.jpg"]}
-        interval={6000}
-        heightClass="min-h-[720px] md:min-h-[620px] lg:min-h-[680px]"
-      >
-        <Hero />
-      </RotatingHero>
+  images={[
+    "/BG-IMG-1.jpg",
+    "/BG-IMG-2.jpg",
+    "/BG-IMG-3.jpg",
+    "/BG-IMG-4.jpg",
+    "/BG-IMG-5.jpg",
+  ]}
+  interval={6000}
+  /* ⬇️ Yahi se height control hoti hai */
+  heightClass="min-h-[650px] md:min-h-[800px] lg:min-h-[920px]"
+>
+  <Hero />
+</RotatingHero>
+
 
       {/* ✅ No manual heading here; component will render its own section */}
       <section className="max-w-6xl mx-auto p-6">
