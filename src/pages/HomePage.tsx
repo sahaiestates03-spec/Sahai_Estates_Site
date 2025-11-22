dbbb83e2bfcdbbfb6e4eb83a915fa186e1a06e78
sahaiestates03-spec <sahaiestates03@gmail.com>
2025-11-22 17:08:32 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,31 @@
 import RotatingHero from "../components/RotatingHero";
-import Hero from '../components/Hero';
-import FeaturedProperties from '../components/FeaturedProperties';
-import About from '../components/About';
-import Testimonials from '../components/Testimonials';
-import ContactForm from '../components/ContactForm';
+import Hero from "../components/Hero";
+import FeaturedProperties from "../components/FeaturedProperties";
+import About from "../components/About";
+import Testimonials from "../components/Testimonials";
+import ContactForm from "../components/ContactForm";
 import HomeSearch from "../components/HomeSearch";
 import FeaturedNewLaunch from "../components/FeaturedNewLaunch";
 
 export default function HomePage() {
   return (
     <div className="overflow-x-hidden">
-      <RotatingHero
-  images={[
-    "/BG-IMG-3.jpg",
-    "/BG-IMG-6.jpg",
-    "/BG-IMG-7.jpg",
-    "/BG-IMG-8.jpg",
-    "/BG-IMG-9.jpg",
-    "/BG-IMG-10.jpg",
-    "/BG-IMG-11.jpg",
-  ]}
-  interval={6000}
-  /* ⬇️ Yahi se height control hoti hai */
-  heightClass="min-h-[650px] md:min-h-[800px] lg:min-h-[920px]"
->
-  {/* HERO + SEARCH ALWAYS ON TOP */}
+      {/* HERO + SEARCH ALWAYS ON TOP */}
       <div className="relative z-[50]">
-        <RotatingHero />
-        {/* agar HomeSearch yahan separately call kar rahe ho to:
-        <HomeSearch />
-        */}
+        <RotatingHero
+          images={[
+            "/BG-IMG-3.jpg",
+            "/BG-IMG-6.jpg",
+            "/BG-IMG-7.jpg",
+            "/BG-IMG-8.jpg",
+            "/BG-IMG-9.jpg",
+            "/BG-IMG-10.jpg",
+            "/BG-IMG-11.jpg",
+          ]}
+          interval={6000}
+          /* ⬇️ Yahi se height control hoti hai */
+          heightClass="min-h-[650px] md:min-h-[800px] lg:min-h-[920px]"
+        />
       </div>
 
       {/* FEATURED NEW LAUNCHES THODA PEECHE */}
@@ -37,8 +33,7 @@ export default function HomePage() {
         <FeaturedNewLaunch />
       </div>
 
-
-      {/* ✅ No manual heading here; component will render its own section */}
+      {/* ✅ Featured properties section */}
       <section className="max-w-6xl mx-auto p-6">
         <FeaturedProperties />
       </section>
